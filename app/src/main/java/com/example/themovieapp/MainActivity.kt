4578a78477c6c48269bae82af60dc04d9e9870c0
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-05-26 15:43:10 +0300

    Deleted Unused import directives

--- app/src/main/java/com/example/themovieapp/MainActivity.kt
+++ app/src/main/java/com/example/themovieapp/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.themovieapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.themovieapp.databinding.ActivityMainBinding
