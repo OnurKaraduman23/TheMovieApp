f38ad1bfe3a913b5ccced8a023cd08b89c0c4528
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-05-27 23:47:25 +0300

    Added Use Case for New Movies

---
+++ app/src/main/java/com/example/themovieapp/domain/use_case/GetNewMoviesUseCase.kt
@@ -0,0 +1,26 @@
+package com.example.themovieapp.domain.use_case
+
+import com.example.themovieapp.common.Resource
+import com.example.themovieapp.common.extension.tryFlowOrEmitError
+import com.example.themovieapp.data.mapper.toUIModel
+import com.example.themovieapp.di.IoDispatcher
+import com.example.themovieapp.domain.model.NewMovieUIModel
+import com.example.themovieapp.domain.repository.MoviesRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+class GetNewMoviesUseCase @Inject constructor(
+    private val moviesRepository: MoviesRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+) {
+
+    fun getNewMovies(): Flow<Resource<List<NewMovieUIModel>>> =
+        tryFlowOrEmitError(dispatcher) {
+            // NewMoviesDto -> NewMoviesUIModel dönüşümünü kullanarak
+            val newMoviesDto = moviesRepository.getNewMovies()
+            val newMoviesUIModel = newMoviesDto.toUIModel()
+            newMoviesUIModel.results  // Bu satır, NewMoviesUIModel içindeki NewMovieUIModel listesini döndürür
+        }
+}
