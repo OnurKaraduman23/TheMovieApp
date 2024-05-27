cce822b3879e6740a1c6e917b13deaea67d387ab
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-05-27 23:47:48 +0300

    Added Constants for api (BASE_URL, API_KEY and image operations)

---
+++ app/src/main/java/com/example/themovieapp/common/Constants.kt
@@ -0,0 +1,21 @@
+package com.example.themovieapp.common
+
+object Constants {
+
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "7c646d0a3ce431c66c2408250dbc2bfe"
+    const val STARTING_PAGE_INDEX = 1
+
+
+    const val QUERY_API_KEY = "api_key"
+    const val QUERY_PAGE = "page"
+
+    const val IMAGES_BASE_URL = "https://image.tmdb.org/t/p/"
+    const val IMAGE_200 = "w200/"
+    const val IMAGE_400 = "w400/"
+    const val IMAGE_500 = "w500/"
+
+
+
+
+}
