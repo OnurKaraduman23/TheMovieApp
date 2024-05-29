feb60fd58042ee36281e384a6b15a7b46abaa91f
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-05-30 02:00:19 +0300

    Deleted unnecessary onEach Scope

--- app/src/main/java/com/example/themovieapp/domain/use_case/GetPopularMoviesUseCase.kt
+++ app/src/main/java/com/example/themovieapp/domain/use_case/GetPopularMoviesUseCase.kt
@@ -1,15 +1,12 @@
 package com.example.themovieapp.domain.use_case
 
-import android.util.Log
 import androidx.paging.PagingData
-import androidx.paging.map
 import com.example.themovieapp.di.IoDispatcher
 import com.example.themovieapp.domain.model.ui_model.popular_movies.PopularMovieUIModel
 import com.example.themovieapp.domain.repository.MoviesRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class GetPopularMoviesUseCase @Inject constructor(
@@ -20,8 +17,4 @@ class GetPopularMoviesUseCase @Inject constructor(
     suspend fun getPopularMovies(): Flow<PagingData<PopularMovieUIModel>> =
         moviesRepository.getPopularMovies()
             .flowOn(dispatcher)
-            .onEach { pagingData ->
-                pagingData.map { movie ->
-                }
-            }
 }
