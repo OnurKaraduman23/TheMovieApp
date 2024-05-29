a20bdc5c0d759eea1ebf776b22fc750f943840d4
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-05-30 01:55:34 +0300

    Added Paging Libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.paging.common.android)
+    implementation(libs.androidx.leanback.paging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
