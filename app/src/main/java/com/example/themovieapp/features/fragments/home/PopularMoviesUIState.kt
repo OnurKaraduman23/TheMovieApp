d8b484cf1d4211263f65b4e992c9b2ba75d7be13
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-05-30 01:55:43 +0300

    Deleted unnecessary UIState for paging

--- app/src/main/java/com/example/themovieapp/features/fragments/home/PopularMoviesUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.themovieapp.features.fragments.home
-
-import com.example.themovieapp.domain.model.ui_model.new_movies.NewMovieUIModel
-import com.example.themovieapp.domain.model.ui_model.popular_movies.PopularMovieUIModel
-
-data class PopularMoviesUIState(
-    val isLoading: Boolean = false,
-    val popularMoviesList: List<PopularMovieUIModel> = emptyList()
-)
