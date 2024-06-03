b93fed02802cbec67fb36812c067cd16eaaf9c9f
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-06-04 01:54:10 +0300

    Added Favorites Data Source

---
+++ app/src/main/java/com/example/themovieapp/data/local/favorite_movies/datasource/FavoritesDataSource.kt
@@ -0,0 +1,36 @@
+package com.example.themovieapp.data.local.favorite_movies.datasource
+
+import com.example.themovieapp.data.local.favorite_movies.MovieDao
+import com.example.themovieapp.data.local.favorite_movies.MovieEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class FavoritesDataSource @Inject constructor(
+    private val movieDao: MovieDao
+) {
+
+    suspend fun addFavorites(
+        movieId: Int,
+        genreIds: String,
+        posterPath: String,
+        overview: String,
+        releaseDate: String,
+        title: String,
+        voteAverage: Double
+    ) {
+        val newFav = MovieEntity(
+            0,
+            movieId = movieId,
+            genreIds = genreIds,
+            posterPath = posterPath,
+            overview = overview,
+            releaseDate = releaseDate,
+            title = title,
+            voteAverage = voteAverage
+        )
+        movieDao.addFavorite(newFav)
+    }
+
+    fun getFavorites(): Flow<List<MovieEntity>> =
+        movieDao.getFavorites()
+}
