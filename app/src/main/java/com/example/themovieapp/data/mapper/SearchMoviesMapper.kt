d25e0a3b1a27c84a43ca7e401a8ada8712340b22
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-06-03 15:36:02 +0300

    Deleted redundant convert type (toString)

--- app/src/main/java/com/example/themovieapp/data/mapper/SearchMoviesMapper.kt
+++ app/src/main/java/com/example/themovieapp/data/mapper/SearchMoviesMapper.kt
@@ -1,12 +1,8 @@
 package com.example.themovieapp.data.mapper
 
 
-import com.example.themovieapp.data.remote.dto.popular_movies.PopularMoviesDto
-import com.example.themovieapp.data.remote.dto.popular_movies.PopularMoviesResultDto
 import com.example.themovieapp.data.remote.dto.search_movie.SearchMovieDto
 import com.example.themovieapp.data.remote.dto.search_movie.SearchMoviesDto
-import com.example.themovieapp.domain.model.ui_model.popular_movies.PopularMovieUIModel
-import com.example.themovieapp.domain.model.ui_model.popular_movies.PopularMoviesUIModel
 import com.example.themovieapp.domain.model.ui_model.search_movie.SearchMovieUIModel
 import com.example.themovieapp.domain.model.ui_model.search_movie.SearchMoviesUIModel
 import java.util.Locale
@@ -18,7 +14,7 @@ fun SearchMovieDto.toUIModel(): SearchMovieUIModel {
         title = this.title,
         posterPath = this.posterPath,
         voteAverage = try {
-            String.format(Locale.US, "%,.1f", this.voteAverage).toString()
+            String.format(Locale.US, "%,.1f", this.voteAverage)
         } catch (e: Exception) {
             "Unknown"
         },
