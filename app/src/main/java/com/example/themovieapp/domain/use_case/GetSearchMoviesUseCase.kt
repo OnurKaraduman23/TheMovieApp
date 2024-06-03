bb5806ba7e332d9280c16fb45a775b5ef150e075
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-06-03 15:32:33 +0300

    Added UseCase for Search operation

---
+++ app/src/main/java/com/example/themovieapp/domain/use_case/GetSearchMoviesUseCase.kt
@@ -0,0 +1,21 @@
+package com.example.themovieapp.domain.use_case
+
+import androidx.paging.PagingData
+import com.example.themovieapp.di.IoDispatcher
+import com.example.themovieapp.domain.model.ui_model.popular_movies.PopularMovieUIModel
+import com.example.themovieapp.domain.model.ui_model.search_movie.SearchMovieUIModel
+import com.example.themovieapp.domain.repository.MoviesRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class GetSearchMoviesUseCase @Inject constructor(
+    private val moviesRepository: MoviesRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+) {
+
+    suspend fun getSearchMovies(query: String): Flow<PagingData<SearchMovieUIModel>> =
+        moviesRepository.getSearchMovies(query)
+            .flowOn(dispatcher)
+}
